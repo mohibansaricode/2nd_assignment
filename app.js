0650ca4391b6723976cb5ac7c9b702e3a5cca2e6
mohibansaricode <mmcode46@gmail.com>
2024-10-22 19:46:20 +0500

    my javascript 2nd assignment

---
+++ app.js
@@ -0,0 +1,21 @@
+// 1 , Write a function that creates a closure and returns a function that can add
+// a specific number to any number passed to it. For example, if the closure is
+// created with 5, the returned function should add 5 to any number passed
+// to it.
+
+
+// function sum(addNumber) {
+//     return function (newNumber) {
+//         console.log(addNumber + newNumber)
+//     }
+// }
+
+// let sumNumber = sum(5);
+// let sumNumber2 = sum(5);
+
+
+// sumNumber(10);
+// sumNumber2(5);
+
+
+
